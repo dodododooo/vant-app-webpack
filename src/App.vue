256977448d0d3599cc516c699aa164a36db3a148
gongzheng <meme_456@163.com>
2021-09-22 16:40:31 +0800

    store

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view v-slot="{ Component, route }">
     <transition :name="route.meta.transition || 'fade'" mode="out-in">
       <keep-alive :include="aliveComponents">
@@ -12,21 +8,16 @@
       </keep-alive>
     </transition>
   </router-view>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent } from 'vue';
 import { useStore } from '@/store';
 
 export default defineComponent({
   name: 'App',
   setup() {
     const store = useStore();
-    const name = computed(() => store.state.user.name)
     return {
       aliveComponents: store.state.keepAliveComponents
     }
